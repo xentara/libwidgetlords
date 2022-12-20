9dea00f477ac179aba7fe08a22b2495b934954de
Fatima Waheed <fatima.waheed@emocean.io>
2022-12-20 09:58:40 +0100

    added RaspberryPI overlay that includes all supported chip select lines

--- overlays/overlay.sh
+++ overlays/overlay.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 dtc -@ -I dts -O dtb -o pi-spi.dtbo pi-spi.dts
+dtc -@ -I dts -O dtb -o pi-spi-full.dtbo pi-spi-full.dts
 dtc -@ -I dts -O dtb -o vpe-2701c.dtbo vpe-2701c.dts
 dtc -@ -I dts -O dtb -o vpe-2901a.dtbo vpe-2901a.dts
 dtc -@ -I dts -O dtb -o vpe-3011b.dtbo vpe-3011b.dts
